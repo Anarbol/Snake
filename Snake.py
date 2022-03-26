9a72295a34e316e1de3de630ed9a09ab9b07dfac
Caro González <102176621+CaroGlez15@users.noreply.github.com>
2022-03-26 00:52:52 -0600

    Cambio de color

--- Snake.py
+++ Snake.py
@@ -6,6 +6,10 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+colors = ['black', 'yellow', 'pink', 'purple', 'blue']
+randcolor = choice(colors)
+colors2 =['green', 'grey', 'brown', 'orange', 'aqua']
+randcolor2 = choice(colors2)
 
 
 def change(x, y):
@@ -43,9 +47,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, 'randcolor')
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'randcolor2')
     update()
     ontimer(move, 100)
 
